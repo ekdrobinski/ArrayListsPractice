8fb1d4af363e1c124683fb51d1937ea78714392c
ekdrobinski <127761998+ekdrobinski@users.noreply.github.com>
2023-05-11 08:26:38 -0500

    Update HowLarge.java
    I will need to practice pushing from intelliJ more.

--- src/HowLarge.java
+++ src/HowLarge.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class HowLarge {
+import java.util.ArrayList;
+import java.util.Scanner;
+public class HowLarge {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<String>();
+        String input = "";
+        while (true) {
+            System.out.print("Enter a string. Enter an empty string to quit.");
+            input = scanner.nextLine();
+            if (input.equals("")) {
+                break;
+            }
+            list.add(input);
+        }
+        System.out.println("The total amount of items in the list was: " + list.size());
+    }
 }

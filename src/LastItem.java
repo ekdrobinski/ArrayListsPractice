95b071a708b78b7da6ff8b5d352db1f61477c062
ekdrobinski <127761998+ekdrobinski@users.noreply.github.com>
2023-05-11 08:27:34 -0500

    Update LastItem.java

--- src/LastItem.java
+++ src/LastItem.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class LastItem {
+import java.util.Scanner;
+public class LastItem {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String lastString = "something";
+
+        System.out.println("Enter strings (press Enter without typing anything to finish):");
+
+        while (true) {
+            System.out.print("Enter a string (or press Enter to finish): ");
+            String input = scanner.nextLine();
+            if (input.isEmpty()) {
+                break;
+            }
+            lastString = input;
+        }
+
+        if (lastString != "something") {
+            System.out.println("The last string entered was: " + lastString);
+        } else {
+            System.out.println("No strings were entered.");
+        }
+    }
+
 }
